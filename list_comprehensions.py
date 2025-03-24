5e20a23b537f2013c5a76f58ba8d6d8c0759c519
Cesar Dushimimana <dushimezar2003@gmail.com>
2025-03-24 21:52:01 +0100

    list comprehensions with condition

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -1,4 +1,17 @@
 values = []
 # Using a for loop to create a list of values
-for x in range(10):
-    values.append(x * 2)
+# for x in range(10):
+#     values.append(x * 2)
+
+
+# Using a list comprehension to create a list of values
+values = [x * 2 for x in range(10)]
+# print(values)
+
+# Print all even and odd numbers separately from 0 to 50
+
+even_numbers = [x for x in range(51) if x % 2 == 0]
+odd_numbers = [x for x in range(51) if x % 2 != 0]
+print(f"Even numbers:\n {even_numbers}")
+print(f"Odd numbers:\n {odd_numbers}")
+
