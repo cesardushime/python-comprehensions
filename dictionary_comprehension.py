c50594ba01eec538af2dac01b00d0afa34b38186
Cesar Dushimimana <dushimezar2003@gmail.com>
2025-03-24 23:00:39 +0100

    creating a dictionary with comprehension and zip method

---
+++ dictionary_comprehension.py
@@ -0,0 +1,10 @@
+# Creating a dictionary with comprehension
+
+# Matching students with their grades 
+
+students = ['Alice', 'Bob', 'Charlie', 'David']
+grades = ['B', 'A', 'C', 'B']
+
+# using comprehension to create a dictionary
+
+student_grades = {student: grade for student, grade in zip(students, grades)}
